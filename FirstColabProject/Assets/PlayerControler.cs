635a0e73b4dd8ed0c981e8cef10eaa42aa05f8f3
lefex-v2 <146823569+lefex-v2@users.noreply.github.com>
2024-01-17 13:31:45 +0100

    Update PlayerControler.cs

--- FirstColabProject/Assets/PlayerControler.cs
+++ FirstColabProject/Assets/PlayerControler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    public Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
